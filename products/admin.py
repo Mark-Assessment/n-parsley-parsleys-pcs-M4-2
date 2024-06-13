f911e81e40e5c7b421e3761d8018b7622efaea82
Natte2110 <natteparsley@gmail.com>
2024-06-13 17:58:26 +0100

    feat: Register models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Brand, CPU, GPU, PSU, RAM, Motherboard
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(CPU)
+admin.site.register(GPU)
+admin.site.register(PSU)
+admin.site.register(RAM)
+admin.site.register(Motherboard)
