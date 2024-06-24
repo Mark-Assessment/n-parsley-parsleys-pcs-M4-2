86702ad361a2fd1a63d43d21e40e2280dcb20d45
Natte2110 <natteparsley@gmail.com>
2024-06-24 15:40:01 +0100

    feat: Set path for updating currency for the session

--- parsleypcs/urls.py
+++ parsleypcs/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from home.views import set_currency
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
+    path('set_currency/<str:currency>/', set_currency, name='set_currency'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
