85bcb1797cfc3c098dfcfa8fa0ce814d9b77cab6
Natte2110 <natteparsley@gmail.com>
2024-06-24 13:32:15 +0100

    feat: Add remaining models to admin page

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Brand, CPU, GPU, PSU, RAM, Motherboard
+from .models import Category, Brand, CPU, GPU, PSU, RAM, Motherboard, CPUSocket, RAMTechnology, FormFactor, Product
 
 admin.site.register(Category)
 admin.site.register(Brand)
@@ -10,3 +10,7 @@ admin.site.register(GPU)
 admin.site.register(PSU)
 admin.site.register(RAM)
 admin.site.register(Motherboard)
+admin.site.register(CPUSocket)
+admin.site.register(RAMTechnology)
+admin.site.register(FormFactor)
+admin.site.register(Product)
