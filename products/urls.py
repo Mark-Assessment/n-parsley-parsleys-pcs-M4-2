5acab2c4b7806e6e4d924c5867ab4402734cc97d
Natte2110 <natteparsley@gmail.com>
2024-06-24 15:09:46 +0100

    feat: Add url for product detail page

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('all-products', views.all_products, name='all_products'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
