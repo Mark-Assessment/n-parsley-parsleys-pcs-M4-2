70f46d4aea937c61d04dcd873bd533a33676c73d
Natte2110 <natteparsley@gmail.com>
2024-06-24 15:07:23 +0100

    feat: Add model for cart items

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchased = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.product.price * self.quantity
+        super().save(*args, **kwargs)
