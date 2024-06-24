502ca671e7394dcf4a790a2bcfd392b2c73822c9
Natte2110 <natteparsley@gmail.com>
2024-06-24 15:09:32 +0100

    feat: Add view function for viewing the product detailed version

--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'products/all_products.html', {'products': products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'products/product_detail.html', {'product': product})
