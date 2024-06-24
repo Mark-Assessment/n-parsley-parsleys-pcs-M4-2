544f338036c16eacfee13a18ebc44db76ee081a0
Natte2110 <natteparsley@gmail.com>
2024-06-24 15:36:33 +0100

    feat: Add aditional conversions to include symbols

--- home/templatetags/currency.py
+++ home/templatetags/currency.py
@@ -1,10 +1,25 @@
 from django import template
+from decimal import Decimal
 
 register = template.Library()
 
-@register.filter(name='currency')
-def currency(value):
+currency_symbols = {
+    'GBP': '£',
+    'USD': '$',
+    'EUR': '€',
+}
+
+@register.filter(name='convert_price')
+def convert_price(value, currency_info):
     """
-    Formats a number as currency.
+    Converts a price to the specified currency using the conversion rate and symbol.
     """
-    return "£{:,.2f}".format(value)
+    try:
+        currency, conversion_rate = currency_info.split(':')
+        value = Decimal(value)
+        conversion_rate = Decimal(conversion_rate)
+        converted_value = value * conversion_rate
+        symbol = currency_symbols.get(currency, '$')
+        return f"{symbol}{converted_value:,.2f}"
+    except (ValueError, TypeError, InvalidOperation):
+        return value 
