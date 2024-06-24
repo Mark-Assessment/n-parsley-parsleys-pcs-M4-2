1d6cafef97d732723837f06ae65cb1d720647622
Natte2110 <natteparsley@gmail.com>
2024-06-24 15:39:06 +0100

    feat: Add function to handle setting the currency for the session

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
+
+def set_currency(request, currency):
+    request.session['currency'] = currency
+    return redirect(request.META.get('HTTP_REFERER', reverse('home')))
