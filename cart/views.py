21201bc7adb5764cd7c36e9b3fae474774ceb55e
Natte2110 <natteparsley@gmail.com>
2024-07-04 22:49:05 +0100

    fix: Fixed issue with multiple items not being added to cart

--- cart/views.py
+++ cart/views.py
@@ -4,17 +4,22 @@ from django.urls import reverse
 from .models import CartItem
 from products.models import Product
 
+@login_required
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
+    quantity = int(request.POST.get('quantity', 1))
     cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product, purchased=False)
 
     if not created:
-        cart_item.quantity += 1
+        cart_item.quantity += quantity
+    else:
+        cart_item.quantity = quantity
     cart_item.total_price = cart_item.product.price * cart_item.quantity
     cart_item.save()
 
     return redirect(reverse('cart_detail'))
 
+@login_required
 def cart_detail(request):
     cart_items = CartItem.objects.filter(user=request.user, purchased=False)
     return render(request, 'cart/cart_detail.html', {'cart_items': cart_items})
