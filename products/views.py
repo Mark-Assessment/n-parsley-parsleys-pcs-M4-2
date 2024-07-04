0cd46300c91332ce110fa8052a7a82e0c7d94ad9
Natte2110 <natteparsley@gmail.com>
2024-07-04 22:38:10 +0100

    feat: Add filtering by brand and category names instead of IDs

--- products/views.py
+++ products/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category, Brand
 from .forms import ProductFilterForm
 
 def all_products(request):
     products = Product.objects.all()
-    form = ProductFilterForm(request.GET)
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
 
+    # Filter by category name
+    category_name = request.GET.get('category')
+    if category_name:
+        category = get_object_or_404(Category, name=category_name)
+        products = products.filter(category=category)
+
+    # Filter by brand name
+    brand_name = request.GET.get('brand')
+    if brand_name:
+        brand = get_object_or_404(Brand, name=brand_name)
+        products = products.filter(brand=brand)
+
+    form = ProductFilterForm(request.GET or None)
     if form.is_valid():
         if form.cleaned_data['price_min']:
             products = products.filter(price__gte=form.cleaned_data['price_min'])
@@ -15,14 +29,15 @@ def all_products(request):
             products = products.filter(rating__gte=form.cleaned_data['rating_min'])
         if form.cleaned_data['rating_max']:
             products = products.filter(rating__lte=form.cleaned_data['rating_max'])
-        if form.cleaned_data['category']:
-            products = products.filter(category=form.cleaned_data['category'])
-        if form.cleaned_data['brand']:
-            products = products.filter(brand=form.cleaned_data['brand'])
         if form.cleaned_data['sort_by']:
             products = products.order_by(form.cleaned_data['sort_by'])
 
-    return render(request, 'products/all_products.html', {'products': products, 'form': form})
+    return render(request, 'products/all_products.html', {
+        'products': products,
+        'form': form,
+        'categories': categories,
+        'brands': brands,
+    })
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
