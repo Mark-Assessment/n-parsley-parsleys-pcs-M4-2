a8e2ba9b46334f10c302be145bfed708b3b05f22
Natte2110 <natteparsley@gmail.com>
2024-07-10 22:56:26 +0100

    feat: Add conversion rates to session info

--- home/views.py
+++ home/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home/index.html')
 
+
 def set_currency(request, currency):
+    conversion_rates = {
+        'GBP': 1.0,
+        'USD': 1.33,
+        'EUR': 1.13,
+    }
+    conversion_rate = conversion_rates.get(currency, 0.75)
+    request.session['currency_info'] = f'{currency}:{conversion_rate}'
     request.session['currency'] = currency
     return redirect(request.META.get('HTTP_REFERER', reverse('home')))
