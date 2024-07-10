d4723bcb6692af181457476be63e45a3cf12a967
Natte2110 <natteparsley@gmail.com>
2024-07-10 21:01:50 +0100

    feat: add Order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    address_line_1 = models.CharField(max_length=255)
+    address_line_2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100)
+    county = models.CharField(max_length=100, blank=True, null=True)
+    postcode = models.CharField(max_length=10)
+    country = models.CharField(max_length=100, default='United Kingdom')
+    stripe_payment_intent_id = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user}"
