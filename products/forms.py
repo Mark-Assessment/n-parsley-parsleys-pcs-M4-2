804ee4814775304cd0645286566ca69db438bb15
Natte2110 <natteparsley@gmail.com>
2024-07-11 17:58:01 +0100

    refactor: Update form ot include null choices

--- products/forms.py
+++ products/forms.py
@@ -6,10 +6,28 @@ class ProductFilterForm(forms.Form):
     price_max = forms.DecimalField(required=False, label='Max Price', min_value=0)
     rating_min = forms.DecimalField(required=False, label='Min Rating', min_value=0, max_value=5, decimal_places=1)
     rating_max = forms.DecimalField(required=False, label='Max Rating', min_value=0, max_value=5, decimal_places=1)
-    category = forms.ChoiceField(required=False, choices=[(category.name, category.name) for category in Category.objects.all()], label='Category')
-    brand = forms.ChoiceField(required=False, choices=[(brand.name, brand.name) for brand in Brand.objects.all()], label='Brand')
-    sort_by = forms.ChoiceField(required=False, choices=[
-        ('name', 'Name'),
-        ('price', 'Price'),
-        ('rating', 'Rating'),
-    ], label='Sort By')
+    
+    category = forms.ModelChoiceField(
+        required=False, 
+        queryset=Category.objects.all(), 
+        label='Category',
+        empty_label='Select a category'
+    )
+    
+    brand = forms.ModelChoiceField(
+        required=False, 
+        queryset=Brand.objects.all(), 
+        label='Brand',
+        empty_label='Select a brand'
+    )
+    
+    sort_by = forms.ChoiceField(
+        required=False, 
+        choices=[
+            ('', 'Sort By'),
+            ('name', 'Name'),
+            ('price', 'Price'),
+            ('rating', 'Rating'),
+        ], 
+        label='Sort By'
+    )
