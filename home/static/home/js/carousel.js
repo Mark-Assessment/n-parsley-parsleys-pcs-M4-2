6ca9253a4202648dc6bf94a1eb643ea2ef1b07c1
Natte2110 <natteparsley@gmail.com>
2024-07-11 16:22:46 +0100

    refactor: Update carousel configuration

--- home/static/home/js/carousel.js
+++ home/static/home/js/carousel.js
@@ -1,21 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#discounted-products-carousel, #featured-products-carousel").owlCarousel({
         items: 4,
         loop: true,
         margin: 10,
-        nav: true,
+        pagination: false,
+        navigation: true,
+        autoPlay: 3000,
+        stopOnHover: true,
+        navigationText: ["<", ">"],
         responsive: {
             0: {
                 items: 1,
-                nav: true
             },
             600: {
                 items: 2,
-                nav: true
+            },
+            800: {
+                items: 3,
             },
             1000: {
+                items: 3,
+            },
+            1500: {
                 items: 4,
-                nav: true
             }
         }
     });
