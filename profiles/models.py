bc219cac1e18cc76f77a7cc245b5680378298f57
Natte2110 <natteparsley@gmail.com>
2024-07-11 16:44:11 +0100

    feat: Add model for user defaults

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+class UserDefaults(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address_line1 = models.CharField(max_length=255, blank=True, null=True)
+    address_line2 = models.CharField(max_length=255, blank=True, null=True)
+    town_or_city = models.CharField(max_length=255, blank=True, null=True)
+    county = models.CharField(max_length=255, blank=True, null=True)
+    postcode = models.CharField(max_length=20, blank=True, null=True)
+    country = CountryField(blank_label='(select country)', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s defaults"
