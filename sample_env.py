a5f7984ce19c09dbab0ae5b8197bb6ab2bb3cacf
Natte2110 <natteparsley@gmail.com>
2024-07-12 17:24:26 +0100

    feat: Add sample env for users

---
+++ sample_env.py
@@ -0,0 +1,11 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "")
+os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("STRIPE_PUBLIC_KEY", "")
+os.environ.setdefault("STRIPE_SECRET_KEY", "")
+os.environ.setdefault("DATABASE_URL", "")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "")
+os.environ.setdefault("AWS_STORAGE_BUCKET_NAME", "")
+os.environ.setdefault("AWS_S3_REGION_NAME", "")
