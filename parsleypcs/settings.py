627b6e571dae37eadff75a474d82de15def0eaeb
Natte2110 <natteparsley@gmail.com>
2024-08-26 18:38:54 +0100

    feat: Add custom email backend

--- parsleypcs/settings.py
+++ parsleypcs/settings.py
@@ -165,7 +165,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'info@parsleypcs.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_BACKEND = 'home.backends.CustomEmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
